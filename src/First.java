441107dddeded95ecce5414008e46811ec3e6306
Rafeemohd <rafeemd14@gmail.com>
2022-06-12 09:29:12 +0530

    my first git

---
+++ src/First.java
@@ -0,0 +1,17 @@
+
+class First{
+	
+	public static void main(String[] args)
+	{
+	    System.out.println("HELLO Jaava");
+	    //selenium library
+	    //Crome object
+	    //launch url
+	    // Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.
+	    ////a[text()="Today's Deals"]  click
+	    ////h1[text()="Today's Deals"]
+	    //close
+		
+	}
+	
+}	
